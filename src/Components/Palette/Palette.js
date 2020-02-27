42a07243e3b15bb69766f1b176f1269d3a09e499
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-27 07:36:06 +0000

    Addming empty slider

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import ColorBox from "../ColorBox/ColorBox";
 import "./Palette.scss";
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 
 export default class Palette extends Component {
+  
+  handleSliderChange(e)={
+    console.log(e)
+  }
+  
   render() {
     const colorBoxes = this.props.palette.colors[500].map(color => (
       <ColorBox name={color.name} background={color.hex} key={color.hex} />
     ));
     return (
       <div className="Palette">
+        <Slider
+          step={100}
+          min={100}
+          max={900}
+          onChange={this.handleSliderChange}
+        />
         <h2>{this.props.palette.paletteName}</h2>
         <div className="Palette-colors">{colorBoxes}</div>
       </div>
