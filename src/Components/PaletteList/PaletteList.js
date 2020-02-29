d8775ed58ff0d19839d8540a88d20e393c3585eb
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-29 16:28:17 +0000

    GH url fix... try again!

--- src/Components/PaletteList/PaletteList.js
+++ src/Components/PaletteList/PaletteList.js
@@ -37,7 +37,7 @@ const styles = {
 
 class PaletteList extends Component {
   goToPalette = id => {
-    this.props.history.push(`colors-ui/palette/${id}`);
+    this.props.history.push(`/palette/${id}`);
   };
 
   render() {
