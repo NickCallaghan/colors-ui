ed75c0acc3726d55f4250b0da75866c1243d3163
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-29 16:13:45 +0000

    Updates

---
+++ precache-manifest.6d49b47651708564d07fd4951c784ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5130bc72c01eada5210135535413b143",
+    "url": "/colors-ui/index.html"
+  },
+  {
+    "revision": "c49b1bb21c2d6dbbb5a3",
+    "url": "/colors-ui/static/css/2.679c2367.chunk.css"
+  },
+  {
+    "revision": "f2a9938f9ab2f089cfd8",
+    "url": "/colors-ui/static/css/main.56eda403.chunk.css"
+  },
+  {
+    "revision": "c49b1bb21c2d6dbbb5a3",
+    "url": "/colors-ui/static/js/2.f8b5164b.chunk.js"
+  },
+  {
+    "revision": "6194c42717583939b4f76332be3503d4",
+    "url": "/colors-ui/static/js/2.f8b5164b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a9938f9ab2f089cfd8",
+    "url": "/colors-ui/static/js/main.9edaaa9d.chunk.js"
+  },
+  {
+    "revision": "e7c3e76417058821239a",
+    "url": "/colors-ui/static/js/runtime-main.52edfa60.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colors-ui/precache-manifest.03ce78fac34e7eb383ee2dfd13811c3f.js"
+  "/colors-ui/precache-manifest.6d49b47651708564d07fd4951c784ae2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colors-ui/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
