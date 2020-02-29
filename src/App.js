a1c6f46e4dc1a845541f55698de33729864f1aa8
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-29 12:23:11 +0000

    Add Pallet list and Routing to Palette Page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import { Route, Switch } from "react-router-dom";
 import PaletteList from "./Components/PaletteList/PaletteList";
 
 export default class App extends Component {
+  state = {
+    palettes: [...seedColors]
+  };
+
+  findPalette = id => {
+    const thisPalette = this.state.palettes.find(p => p.id === id);
+    return generatePalette(thisPalette);
+  };
+
   render() {
+    console.log(this.findPalette("material-ui-colors"));
     const routes = (
       <Switch>
-        <Route exact path="/" render={() => <PaletteList />} />
         <Route
           exact
-          path="/palette"
-          render={() => <Palette palette={generatePalette(seedColors[0])} />}
+          path="/"
+          render={() => <PaletteList palettes={this.state.palettes} />}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette palette={this.findPalette(routeProps.match.params.id)} />
+          )}
         />
       </Switch>
     );
--- src/Components/PaletteList/PaletteList.js
+++ src/Components/PaletteList/PaletteList.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class PaletteList extends Component {
   render() {
+    const palettes = this.props.palettes.map(p => (
+      <React.Fragment>
+        <Link to={`palette/${p.id}`}>
+          <li>{p.paletteName}</li>
+        </Link>
+      </React.Fragment>
+    ));
     return (
       <div>
         <h1>Palette List</h1>
+        <ul>{palettes}</ul>
       </div>
     );
   }
