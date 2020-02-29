8e751a3ed0054e7888378bb4b77cb4579ad279da
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-29 17:04:05 +0000

    remove subdir from baseurl

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={"/colors-ui"}>
+  <BrowserRouter basename={"/"}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
