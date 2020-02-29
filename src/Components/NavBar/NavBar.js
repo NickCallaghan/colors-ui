ea766418cfac0664a5815df414bd16983b26f88f
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-02-29 06:55:11 +0000

    Import material UI Select and MenuItem

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Slider from "rc-slider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import "rc-slider/assets/index.css";
 import "./NavBar.scss";
