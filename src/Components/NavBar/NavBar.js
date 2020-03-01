ee0da68c16d2ec302646583635fd71d932cb6e80
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-03-01 17:37:38 +0000

    Added level label to navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
+import InputLabel from "@material-ui/core/InputLabel";
 
 import "rc-slider/assets/index.css";
 import "./NavBar.scss";
@@ -20,19 +21,10 @@ export default class NavBar extends Component {
     showSnackBar: false
   };
 
-  handleChangeFormat = e => {
-    this.setState({ format: e.target.value, showSnackBar: true });
-    this.props.changeFormat(e.target.value);
-  };
-
-  dismissSnackBar = () => {
-    this.setState({ showSnackBar: false });
-  };
-
-  render() {
-    const { format } = this.state;
-    const sliderDiv = (
+  renderSlider = () => {
+    return (
       <div className="slider-wrapper">
+        <InputLabel className="slider-label">{`Level ${this.props.colorValue}`}</InputLabel>
         <Slider
           step={100}
           min={100}
@@ -42,8 +34,10 @@ export default class NavBar extends Component {
         />
       </div>
     );
+  };
 
-    const changeFormatDiv = (
+  renderFormatPicker = () => {
+    return (
       <div className="color-format">
         <Select value={this.state.format} onChange={this.handleChangeFormat}>
           <MenuItem value="hex">HEX #FFFFFF</MenuItem>
@@ -52,29 +46,47 @@ export default class NavBar extends Component {
         </Select>
       </div>
     );
+  };
 
+  handleChangeFormat = e => {
+    this.setState({ format: e.target.value, showSnackBar: true });
+    this.props.changeFormat(e.target.value);
+  };
+
+  useSnackBar = () => {
+    const { format } = this.state;
+    return (
+      <Snackbar
+        open={this.state.showSnackBar}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        autoHideDuration={2000}
+        onClose={this.dismissSnackBar}
+        message={<span id="message-id">Format Changed to {format}!</span>}
+        ContentProps={{
+          "aria-describedby": "message-id"
+        }}
+        action={[
+          <IconButton color="inherit" key="close" aria-label="close">
+            <CloseIcon onClick={this.dismissSnackBar} />
+          </IconButton>
+        ]}
+      />
+    );
+  };
+
+  dismissSnackBar = () => {
+    this.setState({ showSnackBar: false });
+  };
+
+  render() {
     return (
       <div className="NavBar">
         <div className="logo">
           <a href="/colors-ui">React Colors</a>
         </div>
-        {this.props.showSlider && sliderDiv}
-        {this.props.allowChangeFormat && changeFormatDiv}
-        <Snackbar
-          open={this.state.showSnackBar}
-          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-          autoHideDuration={2000}
-          onClose={this.dismissSnackBar}
-          message={<span id="message-id">Format Changed to {format}!</span>}
-          ContentProps={{
-            "aria-describedby": "message-id"
-          }}
-          action={[
-            <IconButton color="inherit" key="close" aria-label="close">
-              <CloseIcon onClick={this.dismissSnackBar} />
-            </IconButton>
-          ]}
-        ></Snackbar>
+        {this.props.showSlider && this.renderSlider()}
+        {this.props.allowChangeFormat && this.renderFormatPicker()}
+        {this.useSnackBar}
       </div>
     );
   }
